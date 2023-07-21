ae87ef8dca33ce10bed3dada5b5f329b4980a415
Jingroo <wongjr-wm20@student.tarc.edu.my>
2023-07-21 17:28:15 +0800

    Second commit

--- app/src/main/java/com/example/practical3fri2/MainActivity.kt
+++ app/src/main/java/com/example/practical3fri2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text->view.setBackgroundColor(Color.RED)
             R.id.box_four_text->view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text->view.setBackgroundColor(Color.CYAN)
+            // Boxes using custom colors for background
+            R.id.red_button -> findViewById<TextView>(R.id.box_three_text).setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> findViewById<TextView>(R.id.box_four_text).setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> findViewById<TextView>(R.id.box_five_text).setBackgroundResource(R.color.my_green)
             else-> view.setBackgroundColor(Color.DKGRAY)
         }
     }
@@ -32,10 +37,16 @@ class MainActivity : AppCompatActivity() {
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
-        val clickablesViews : List<View> = listOf(boxOneText,boxTwoText,boxThreeText,boxFourText,boxFiveText,rootConstraintLayout)
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
+        val clickablesViews : List<View> = listOf(boxOneText,boxTwoText,boxThreeText,boxFourText,boxFiveText,rootConstraintLayout,redButton,greenButton,yellowButton)
 
         for(item in clickablesViews){
             item.setOnClickListener{makeColored(it)}
         }
+
+
     }
 }
